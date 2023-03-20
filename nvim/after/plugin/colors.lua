7c4646ba3a2b2e593f823790acdc958b73798a55
Aidan Sorensen <aidanswims00@gmail.com>
2023-03-20 17:04:37 -0400

    i have no idea if this will work so i'm experimenting.

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,13 @@
+function ColorMyPencils(color)
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+	
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+		
+	vim.o.background = "light" -- or "light" for light mode
+	vim.cmd([[colorscheme gruvbox]])
+end
+
+ColorMyPencils()
---
+++ nvim/lua/aidan/init.lua
@@ -0,0 +1,5 @@
+require("aidan.remap")
+require("aidan.packer")
+vim.o.background = "light" -- or "light" for light mode
+
+vim.cmd([[colorscheme gruvbox]])
